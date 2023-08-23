6917968e37c8a8d1857b24bdcbb8739f4db2ad76
JuanSebastian5126 <142518040+JuanSebastian5126@users.noreply.github.com>
2023-08-23 08:47:44 -0500

    Add files via upload

---
+++ PrimerosProgramas/Booleans.java
@@ -0,0 +1,10 @@
+package boolean;
+
+public class Booleans {
+
+    public static void main(String[] args) {
+    boolean intsCanHoldDecimals = false;
+    System.out.println(x:intsCanHoldDecimals);
+    }
+    
+}

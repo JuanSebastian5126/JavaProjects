c0910b74e434db6d170144079a94daf4b8f5bd20
JuanSebastian5126 <142518040+JuanSebastian5126@users.noreply.github.com>
2023-09-06 09:26:07 -0500

    Add files via upload

---
+++ PrimerosProgramas/Tree.java
@@ -0,0 +1,19 @@
+package tree;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        System.out.println("Hey there, I’m Sebastian and I’m learning to code in Java!");
+       System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+       System.out.println("   *   ");
+       System.out.println("  ***  ");
+       System.out.println("*******");
+       System.out.println(" *****  ");
+       System.out.println("  ***  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+       System.out.println("   *  ");
+    }
+    
+}

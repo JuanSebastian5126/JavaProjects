5a07f42f35aa9474137bd7c048b677c5e591cbe0
JuanSebastian5126 <142518040+JuanSebastian5126@users.noreply.github.com>
2023-09-06 09:54:00 -0500

    Add files via upload

---
+++ PrimerosProgramas/CharExamples.java
@@ -0,0 +1,10 @@
+package charexamples;
+
+public class CharExamples {
+
+    public static void main(String[] args) {  
+     char expectedGrade = 'C'; 
+     System.out.println(expectedGrade);
+    }
+    
+}
---
+++ PrimerosProgramas/CreateCompoundOperatorsExample.java
@@ -0,0 +1,23 @@
+package create.compoundoperatorsexample;
+
+//Compound operators:
+/*
+Addition (+=)
+Subtraction (-=)
+Multiplication (*=)
+Division (/=)
+Modulo (%=)
+*/ 
+
+public class CreateCompoundOperatorsExample {
+
+    public static void main(String[] args) {
+        int numCookies = 17;
+    numCookies -= 3;
+    System.out.println(numCookies);
+    numCookies /= 2;
+    // Add your code above
+    System.out.println(numCookies);
+    }
+    
+}
